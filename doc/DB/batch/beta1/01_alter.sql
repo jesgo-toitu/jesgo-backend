4b43b583eac11e9663f6ec4f1f8821c54dd02e29
kosaka-hokuto <shimpei-kosaka@hscnet.co.jp>
2022-08-23 15:41:00 +0900

    VIEW作成用SQL更新

--- doc/DB/batch/beta1/01_alter.sql
+++ doc/DB/batch/beta1/01_alter.sql
@@ -1,3 +1,4 @@
+DROP VIEW view_latest_schema;
 CREATE TABLE IF NOT EXISTS jesgo_search_column
 (
 column_id integer,
@@ -6,6 +7,14 @@ column_name text,
 PRIMARY KEY ( column_id, column_type)
 );
 
+CREATE VIEW view_latest_schema AS 
+SELECT s.* 
+FROM jesgo_document_schema s 
+INNER JOIN 
+(SELECT schema_id, MAX(schema_primary_id) newest_id 
+ FROM jesgo_document_schema GROUP BY schema_id) g 
+ON s.schema_id = g.schema_id 
+WHERE s.schema_primary_id = g.newest_id;
+
 ALTER TABLE jesgo_document_schema ADD COLUMN subschema_default integer[];
-ALTER TABLE jesgo_document_schema ADD COLUMN child_schema_default integer[];
-ALTER VIEW view_latest_schema
+ALTER TABLE jesgo_document_schema ADD COLUMN child_schema_default integer[];50
