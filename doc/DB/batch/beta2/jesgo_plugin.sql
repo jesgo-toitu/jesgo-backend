db89a0319ebe795026eada09855aa3289d8607ad
kosaka-hokuto <shimpei-kosaka@hscnet.co.jp>
2022-12-09 10:09:52 +0900

    プラグインの登録初期値にdeleted=falseを追加

--- doc/DB/batch/beta2/jesgo_plugin.sql
+++ doc/DB/batch/beta2/jesgo_plugin.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS jesgo_plugin
     show_upload_dialog boolean DEFAULT true,
     filter_schema_query text COLLATE pg_catalog."default",
     explain text COLLATE pg_catalog."default",
-    deleted boolean,
+    deleted boolean NOT NULL DEFAULT false,
     registrant integer,
     last_updated timestamp with time zone,
     CONSTRAINT jesgo_plugin_pkey PRIMARY KEY (plugin_id, plugin_name),
