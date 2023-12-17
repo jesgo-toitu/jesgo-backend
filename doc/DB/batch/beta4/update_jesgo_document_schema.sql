a9fa96a70249853353acbeeede37b5af754db2cb
Yasuhiro Yamamoto <mohyama@mohnet.com>
2023-12-17 11:46:18 +0900

    スキーマPR 20231217-requestに対応したupdateを追加

--- doc/DB/batch/beta4/update_jesgo_document_schema.sql
+++ doc/DB/batch/beta4/update_jesgo_document_schema.sql
@@ -18,3 +18,30 @@ UPDATE jesgo_document_schema
     document_schema = REPLACE(document_schema #>> '{}', '/schema/evaluation/', '/schema/evaluations/')::json,
     schema_id_string = REPLACE(schema_id_string, '/schema/evaluation/', '/schema/evaluations/')
   WHERE document_schema #>> '{}' LIKE '%/schema/evaluation/%';
+
+-- update /schema/OV/staging v1.x 所属リンパ節→領域リンパ節
+UPDATE jesgo_document_schema 
+SET 
+document_schema = REPLACE(document_schema::text,'"所属リンパ節"', '"領域リンパ節"')::json
+WHERE
+schema_id_string = '/schema/OV/staging' AND
+version_major = 1 AND
+jsonb_path_exists(document_schema::jsonb, '$.properties.所属リンパ節')
+
+-- /schema/OV/staging v1 ドキュメントの修正
+UPDATE jesgo_document SET
+document = jsonb_strip_nulls(
+  jsonb_set(
+    jsonb_set(document, '{"領域リンパ節"}', document->'所属リンパ節', TRUE),
+    '{"所属リンパ節"}',
+    'null'
+  )
+)
+WHERE 
+document ? '所属リンパ節' AND
+schema_primary_id IN (
+SELECT schema_primary_id FROM jesgo_document_schema
+WHERE
+  schema_id_string = '/schema/OV/staging' AND
+  version_major = 1
+);
